8c490db49ea557f32160cb0307782e86c9c5b650
Divinare <niemijoe@hotmail.com>
2016-03-25 14:26:40 +0200

    fixed character capsule

--- Source/Outsider/OutsiderCharacter.cpp
+++ Source/Outsider/OutsiderCharacter.cpp
@@ -64,9 +64,9 @@ AOutsiderCharacter::AOutsiderCharacter()
 	// Configure character movement
 	GetCharacterMovement()->GravityScale = 2.0f;
 	GetCharacterMovement()->AirControl = 0.80f;
-	GetCharacterMovement()->JumpZVelocity = 1000.f;
+	GetCharacterMovement()->JumpZVelocity = 350.f;
 	GetCharacterMovement()->GroundFriction = 3.0f;
-	GetCharacterMovement()->MaxWalkSpeed = 600.0f;
+	GetCharacterMovement()->MaxWalkSpeed = 120.0f;
 	GetCharacterMovement()->MaxFlySpeed = 600.0f;
 
 	// Lock character motion onto the XZ plane, so the character can't move in or out of the screen
